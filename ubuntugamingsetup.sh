054f23ced4e580624ff1eb881f1028d5307e248b
Joumo <johniscool237@gmail.com>
2022-05-23 04:31:29 +0000

    Update ubuntugamingsetup.sh

--- ubuntugamingsetup.sh
+++ ubuntugamingsetup.sh
@@ -23,35 +23,19 @@ sudo apt-key add winehq.key
 sudo apt-get update && sudo apt-get upgrade
 
 # Installs wine 
-sudo apt-get install --install-recommends winehq-staging wine-staging wine-staging-i386 wine-staging-amd64
+sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
 
-# Install these for better compatibility
-sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 \
-libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
-
-# Installs the NVIDIA Drivers (COMMENT THEM IF AMD/INTEL)
-sudo add-apt-repository ppa:graphics-drivers/ppa
+# Installs the NVIDIA Drivers (COMMENT THEM IF AMD/INTEL VULKAN CAPABLE GPU REQUIRED)
+sudo add-apt-repository ppa:graphics-drivers/ppa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y nvidia-driver-510 libvulkan1 libvulkan1:i386
 
 # Refreshes Packages and Upgrades them
 sudo apt update && sudo apt upgrade
 
-# installs the 465 driver
-sudo apt install nvidia-driver-465
-
-# Installs vulkan support. (YOU MUST NEED A VULKAN GPU)
-sudo apt install libvulkan1:i386
-
 # Installs the AMD AND INTEL Drivers (Comment them if NVIDIA)
-sudo add-apt-repository ppa:kisak/kisak-mesa
+sudo add-apt-repository ppa:kisak/kisak-mesa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt upgrade && sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
 
 # Refreshes Packages and Upgrades Them
 sudo apt update && sudo apt upgrade
-
-# Installs support for 32-bit games
-sudo apt install libgl1-mesa-dri:i386
-
-# Installs support for vulkan
-sudo apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386
 
 # Reboots the machine to apply all changes
 sudo reboot now

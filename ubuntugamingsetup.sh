34cfd0edb58c57660af1d3fc4f2c9ac9a39b09dc
Sahed Hossain <108656189+sahedhossain06@users.noreply.github.com>
2023-12-24 00:06:51 -0800

    Update ubuntugamingsetup.sh

--- ubuntugamingsetup.sh
+++ ubuntugamingsetup.sh
@@ -4,54 +4,32 @@
 sudo dpkg --add-architecture i386
 
 # Adds the wine repo key
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 
-# Comment out for each ubuntu version 21.04, 20.10, 20.04, 19.10, 19.04, 18.10, and 18.04 only
-# sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ hirsute main'
-# sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
-# sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-# sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main'
-# sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ disco main'
-# sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ cosmic main'
-# sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ cosmic main'
-
-# COMMENT OUT IF RUNNING UBUNTU 18.04, enables faudio package.
-# sudo add-apt-repository ppa:cybermax-dexter/sdl2-backport
+# Comment out for each ubuntu version 23.10, 23.04, 22.04, and 20.04
+#sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/mantic/winehq-mantic.sources
+#sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/lunar/winehq-lunar.sources
+#sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+#sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
 
 # Upgrades the system
 sudo apt-get update && sudo apt-get upgrade
 
 # Installs wine 
-sudo apt-get install --install-recommends winehq-staging wine-staging wine-staging-i386 wine-staging-amd64
-
-# Install these for better compatibility
-sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 \
-libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
+sudo apt install --install-recommends winehq-staging
 
 # Installs the NVIDIA Drivers (COMMENT THEM IF AMD/INTEL)
-sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo add-apt-repository ppa:graphics-drivers/ppa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y nvidia-driver-535 libvulkan1 libvulkan1:i386
 
 # Refreshes Packages and Upgrades them
 sudo apt update && sudo apt upgrade
 
-# installs the 465 driver
-sudo apt install nvidia-driver-465
-
-# Installs vulkan support. (YOU MUST NEED A VULKAN GPU)
-sudo apt install libvulkan1:i386
-
 # Installs the AMD AND INTEL Drivers (Comment them if NVIDIA)
-sudo add-apt-repository ppa:kisak/kisak-mesa
+sudo add-apt-repository ppa:kisak/kisak-mesa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt upgrade && sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
 
 # Refreshes Packages and Upgrades Them
 sudo apt update && sudo apt upgrade
-
-# Installs support for 32-bit games
-sudo apt install libgl1-mesa-dri:i386
-
-# Installs support for vulkan
-sudo apt install mesa-vulkan-drivers mesa-vulkan-drivers:i386
 
 # Reboots the machine to apply all changes
 sudo reboot now
